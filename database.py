7e1040d624b620eac9664d565c597e830e29cf2d
vagrant <vagrant@vagrant.vm>
2018-10-11 20:22:43 +0000

    initial commit

---
+++ database.py
@@ -0,0 +1,50 @@
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    price = Column(String(80), nullable = False)
+    image = Column(String(250), nullable = False)
+    description = Column(String(250), nullable = False)
+    catalog = relationship(Catalog)
+    catalog_id = Column(Integer, ForeignKey('catalog.id'))
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'price': self.price,
+            'image': self.image,
+            'description': self.description,
+            'catalog': self.catalog,
+        }
+
+
+class Catalog(Base):
+    __tablename__ = 'catalog'
+    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable = False)
+    image = Column(String(250), nullable = False)
+    tagline = Column(String(250))
+    @property
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'image': self.image,
+            'tagline': self.tagline,
+        }
+
+
+engine = create_engine('sqlite:///restaurantmenu.db')
+Base.metadata.create_all(engine)
+DBSession = sessionmaker(bind = engine)
+session = DBSession()
+restaurants = session.query(Restaurant).all()
