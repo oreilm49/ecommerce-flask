cbe6519b4d811b79011a044a814aed4112e3eccf
vagrant <vagrant@vagrant.vm>
2018-10-14 14:23:56 +0000

    updated admin ui with boostrap styling

--- static/js/main.js
+++ static/js/main.js
@@ -3,4 +3,18 @@ $(function(){
         interval: 10000
       })
 
+        $('.openClose').click(function() {
+            console.log('clicked')
+            var sidebarWidth = document.getElementById("admin-sidebar").offsetWidth;
+            console.log(sidebarWidth);
+            if (sidebarWidth == 0) {
+                document.getElementById("admin-sidebar").style.width = '160px';
+                document.getElementById("main").style.margin = '0 160px 0'
+            } else {
+                document.getElementById("admin-sidebar").style.width = '0';
+                document.getElementById("main").style.margin = '0'
+            }
+
+        } )
+
 })
