94f373abed034448bf20eae09c5e0da4bc5d0c58
vagrant <vagrant@vagrant.vm>
2018-10-18 18:41:47 +0000

    new table structure in db implemented

--- database.py
+++ database.py
@@ -3,12 +3,12 @@ from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy import create_engine
+import sqlalchemy_jsonfield
 
 Base = declarative_base()
 
-
-class Catalog(Base):
-    __tablename__ = 'catalog'
+class Global_catalog(Base):
+    __tablename__ = 'global'
     id = Column(Integer, primary_key = True)
     name = Column(String(80), nullable = False)
     image = Column(String(250), nullable = True)
@@ -22,26 +22,53 @@ class Catalog(Base):
             'tagline': self.tagline,
         }
 
-
-class Product(Base):
-    __tablename__ = 'product'
+class Catalog(Base):
+    __tablename__ = 'catalog'
     id = Column(Integer, primary_key = True)
     name = Column(String(80), nullable = False)
-    price = Column(String(80), nullable = False)
-    image = Column(String(250), nullable = False)
-    description = Column(String(250), nullable = True)
-    catalog = relationship(Catalog)
-    catalog_id = Column(Integer, ForeignKey('catalog.id'))
+    image = Column(String(250), nullable = True)
+    tagline = Column(String(250))
+    global_catalog = relationship(Global_catalog)
+    global_catalog_id = Column(Integer, ForeignKey('global.id'))
     @property
     def serialize(self):
         return {
             'id': self.id,
             'name': self.name,
-            'price': self.price,
             'image': self.image,
-            'description': self.description,
+            'tagline': self.tagline,
+            'global_catalog': self.globalCatalog_id
         }
 
+
+class Product(Base):
+    __tablename__ = 'product'
+    id = Column(Integer, primary_key = True)
+    images = Column(String(250), nullable = False)
+    header = Column(String(250), nullable = False)
+    model = Column(String(250), nullable = False)
+    price = Column(String(80), nullable = False)
+    global_catalog = relationship(Global_catalog)
+    global_catalog_id = Column(Integer, ForeignKey('global.id'))
+    brand = Column(String(250), nullable = False)
+    description = Column(String(250), nullable = True)
+    specs = Column(sqlalchemy_jsonfield.JSONField(), nullable = False)
+    catalog = relationship(Catalog)
+    catalog_id = Column(Integer, ForeignKey('catalog.id'))
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'images': self.images,
+            'header': self.header,
+            'model': self.model,
+            'price': self.price,
+            'global_catalog': self.global_catalog,
+            'brand': self.brand,
+            'description': self.description,
+            'specs': self.specs,
+            'catalog': self.catalog
+        }
 
 class User(Base):
     __tablename__ = 'user'
